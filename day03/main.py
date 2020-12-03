6c3556a2b82da5a1d8f87224f05fd4387d38c419
Rein Hillmann <reinouth@microsoft.com>
2020-12-03 06:14:26 +0000

    Clean up

--- day03/main.py
+++ day03/main.py
@@ -22,13 +22,15 @@ def count_trees(landscape, slope):
     return trees
 
 if __name__ == "__main__":
-    slope = (3, 1)
-    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     test_data = load_data("test.txt")
     actual_data = load_data("actual.txt")
 
+    # Part 1
+    slope = (3, 1)
     assert count_trees(test_data, slope) == 7
     print(count_trees(actual_data, slope))
 
+    # Part 2
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     assert numpy.prod([count_trees(test_data, s) for s in slopes]) == 336
     print(numpy.prod([count_trees(actual_data, s) for s in slopes]))
